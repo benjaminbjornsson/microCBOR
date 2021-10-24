7711a2f29ebea767d929f74785ce33d901062423
Benjamin Björnsson <benjaminbjornsson@gmail.com>
2021-10-24 10:23:48 +0200

    Add tests of append function in main

--- src/main.c
+++ src/main.c
@@ -3,11 +3,32 @@
 #include "decoder.h"
 #include "encoder.h"
 #include "utils.h"
+#include "dataitem.h"
 
-uint8_t array[] = { 0xA3, 0x01, 0x82, 0x61, 0x61, 0x61, 0x62, 0x02, 0x63, 0x61, 0x62, 0x63, 0x03, 0xA2, 0x61, 0x61, 0x18, 0x7B, 0x61, 0x62, 0x19, 0x01, 0x41 };
+uint8_t integer[] = { 0x01 };
+uint8_t string[] = { 0x61, 0x61 };
+uint8_t array[] = { 0x83, 0x01, 0x02, 0x03 };
+uint8_t map[] = { 0xA2, 0x01, 0x61, 0x61, 0x02, 0x61, 0x62 };
 
 int main() {
-	DataItem *dataItem = decode(array);
-	uint8_t *cbor = encode(dataItem);
-	dbg_buff_print(cbor, dataItem->byteCount);
+	DataItem *dataItem1 = decode(integer);
+	dbg_buff_print(encode(dataItem1), dataItem1->byteCount);
+
+	DataItem *dataItem2 = decode(string);
+	dbg_buff_print(encode(dataItem2), dataItem2->byteCount);
+
+	DataItem *dataItem3 = decode(array);
+	dbg_buff_print(encode(dataItem3), dataItem3->byteCount);
+
+	DataItem *dataItem4 = decode(map);
+	dbg_buff_print(encode(dataItem4), dataItem4->byteCount);
+
+	dataItemAppendElement(dataItem3, dataItem1);
+	dbg_buff_print(encode(dataItem3), dataItem3->byteCount);
+
+	dataItemAppendElement(dataItem3, dataItem2);
+	dbg_buff_print(encode(dataItem3), dataItem3->byteCount);
+
+	dataItemAppendElement(dataItem3, dataItem4);
+	dbg_buff_print(encode(dataItem3), dataItem3->byteCount);
 }
