29a4b815a166285a1e3029e5ea72c4d85fe7e4af
Benjamin Björnsson <benjaminbjornsson@gmail.com>
2021-10-26 16:22:49 +0200

    Add utility byte array compare function

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,5 @@
 #include <stdint.h>
 
 void dbg_buff_print(uint8_t *buff, int count);
+
+bool compareByteArray(uint8_t *first, uint8_t *second, int count);
--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "utils.h"
 
 void dbg_buff_print(uint8_t *buff, int count) {
@@ -7,4 +8,13 @@ void dbg_buff_print(uint8_t *buff, int count) {
 		printf("%02X ", *buff++);
 	}
 	printf("\n");
+}
+
+bool compareByteArray(uint8_t *first, uint8_t *second, int count) {
+	for(int i = 0; i < count; i++) {
+		if(first[i] != second[i])
+			return false;
+	}
+
+	return true;
 }
