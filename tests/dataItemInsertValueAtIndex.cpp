2eb3f1d80620bbdf735555ffb7ae69901f3bf642
Benjamin Björnsson <benjaminbjornsson@gmail.com>
2021-10-26 08:50:45 +0200

    Add more tests for inserting in array

--- tests/dataItemInsertValueAtIndex.cpp
+++ tests/dataItemInsertValueAtIndex.cpp
@@ -17,9 +17,11 @@ uint8_t tagCbor[] = { 0xC0, 0x00 };
 uint8_t specialCbor[] = { 0xF4 };
 
 uint8_t arrayCbor[] = { 0x80 };
-uint8_t finalCbor[] = { 0x82, 0x14, 0x2B, /* 0x42, 0x78, 0x79, 0x43, 0x61, 0x62, 0x63, 0x62, 0x78, 0x79, 0x63, 0x61, 0x62, 0x63, 0xC0, 0x00, 0xF4 */ };
+uint8_t finalZeroToSevenCbor[]	= { 0x88, 0x14, 0x2B, 0x42, 0x78, 0x79, 0x43, 0x61, 0x62, 0x63, 0x62, 0x78, 0x79, 0x63, 0x61, 0x62, 0x63, 0xC0, 0x00, 0xF4 };
+uint8_t finalSevenToZeroCbor[]	= { 0x88, 0xF4, 0xC0, 0x00, 0x63, 0x61, 0x62, 0x63, 0x62, 0x78, 0x79, 0x43, 0x61, 0x62, 0x63, 0x42, 0x78, 0x79, 0x2B, 0x14 };
+uint8_t finalRandomCbor[]		= { 0x88, 0x42, 0x78, 0x79, 0x14, 0xC0, 0x00, 0x43, 0x61, 0x62, 0x63, 0x63, 0x61, 0x62, 0x63, 0x2B, 0x62, 0x78, 0x79, 0xF4 };
 
-TEST(dataItemInsertItemAtIndexTest, dataItemInsertItemAtIndex) {
+TEST(dataItemInsertItemAtIndexTest, dataItemInsertItemAtIndexZeroToSeven) {
 	DataItem *unsignedIntegerItem = decode(unsignedIntegerCbor);
 	DataItem *negativeIntegerItem = decode(negativeIntegerCbor);
 	DataItem *utf8String1Item = decode(utf8String1Cbor);
@@ -32,20 +34,67 @@ TEST(dataItemInsertItemAtIndexTest, dataItemInsertItemAtIndex) {
 	DataItem *arrayItem = decode(arrayCbor);
 	dataItemInsertElementAtIndex(arrayItem, unsignedIntegerItem, 0);
 	dataItemInsertElementAtIndex(arrayItem, negativeIntegerItem, 1);
-	/*
+	dataItemInsertElementAtIndex(arrayItem, utf8String1Item, 2);
+	dataItemInsertElementAtIndex(arrayItem, utf8String2Item, 3);
+	dataItemInsertElementAtIndex(arrayItem, byteString1Item, 4);
+	dataItemInsertElementAtIndex(arrayItem, byteString2Item, 5);
+	dataItemInsertElementAtIndex(arrayItem, tagItem, 6);
+	dataItemInsertElementAtIndex(arrayItem, specialItem, 7);
+
+	uint8_t *final = encode(arrayItem);
+	for(int i = 0; i < sizeof(finalZeroToSevenCbor); i++) {
+		EXPECT_EQ(final[i], finalZeroToSevenCbor[i]);
+	}
+}
+
+TEST(dataItemInsertItemAtIndexTest, dataItemInsertItemAtIndexSevenToZero) {
+	DataItem *unsignedIntegerItem = decode(unsignedIntegerCbor);
+	DataItem *negativeIntegerItem = decode(negativeIntegerCbor);
+	DataItem *utf8String1Item = decode(utf8String1Cbor);
+	DataItem *utf8String2Item = decode(utf8String2Cbor);
+	DataItem *byteString1Item = decode(byteString1Cbor);
+	DataItem *byteString2Item = decode(byteString2Cbor);
+	DataItem *tagItem = decode(tagCbor);
+	DataItem *specialItem = decode(specialCbor);
+
+	DataItem *arrayItem = decode(arrayCbor);
+	dataItemInsertElementAtIndex(arrayItem, unsignedIntegerItem, 0);
+	dataItemInsertElementAtIndex(arrayItem, negativeIntegerItem, 0);
 	dataItemInsertElementAtIndex(arrayItem, utf8String1Item, 0);
 	dataItemInsertElementAtIndex(arrayItem, utf8String2Item, 0);
 	dataItemInsertElementAtIndex(arrayItem, byteString1Item, 0);
 	dataItemInsertElementAtIndex(arrayItem, byteString2Item, 0);
-	*/
-	/*
-	dataItemInsertElementAtIndex(arrayItem, tagItem, 2);
-	dataItemInsertElementAtIndex(arrayItem, specialItem, 3);
-	*/
+	dataItemInsertElementAtIndex(arrayItem, tagItem, 0);
+	dataItemInsertElementAtIndex(arrayItem, specialItem, 0);
 
 	uint8_t *final = encode(arrayItem);
-	for(int i = 0; i < sizeof(finalCbor); i++) {
-		std::cout << "i = " << i << "\n";
-		EXPECT_EQ(final[i], finalCbor[i]) << "%d " << final[i];
+	for(int i = 0; i < sizeof(finalSevenToZeroCbor); i++) {
+		EXPECT_EQ(final[i], finalSevenToZeroCbor[i]);
 	}
+}
+
+TEST(dataItemInsertItemAtIndexTest, dataItemInsertItemAtIndexRandom) {
+	DataItem *unsignedIntegerItem = decode(unsignedIntegerCbor);
+	DataItem *negativeIntegerItem = decode(negativeIntegerCbor);
+	DataItem *utf8String1Item = decode(utf8String1Cbor);
+	DataItem *utf8String2Item = decode(utf8String2Cbor);
+	DataItem *byteString1Item = decode(byteString1Cbor);
+	DataItem *byteString2Item = decode(byteString2Cbor);
+	DataItem *tagItem = decode(tagCbor);
+	DataItem *specialItem = decode(specialCbor);
+
+	DataItem *arrayItem = decode(arrayCbor);
+	dataItemInsertElementAtIndex(arrayItem, unsignedIntegerItem, 0);
+	dataItemInsertElementAtIndex(arrayItem, negativeIntegerItem, 1);
+	dataItemInsertElementAtIndex(arrayItem, utf8String1Item, 0);
+	dataItemInsertElementAtIndex(arrayItem, utf8String2Item, 2);
+	dataItemInsertElementAtIndex(arrayItem, byteString1Item, 4);
+	dataItemInsertElementAtIndex(arrayItem, byteString2Item, 3);
+	dataItemInsertElementAtIndex(arrayItem, tagItem, 2);
+	dataItemInsertElementAtIndex(arrayItem, specialItem, 7);
+
+	uint8_t *final = encode(arrayItem);
+	for(int i = 0; i < sizeof(finalRandomCbor); i++) {
+		EXPECT_EQ(final[i], finalRandomCbor[i]);
+}
 }
